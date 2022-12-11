daf08a70908e8062549c5ef824b32d590e29c8d3
JohnWick92 <grumec92@gmail.com>
2022-12-11 19:28:43 -0300

    Create express server

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express, { Response, Request } from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use(express.json())
+
+app.use((err: Error, resquest: Request, response: Response) => {
+  if (err instanceof Error)
+    return response.status(400).json({ error: err.message })
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  })
+})
+
+export default app
