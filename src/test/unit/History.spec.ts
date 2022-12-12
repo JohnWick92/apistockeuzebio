c1bdc88569a2baa62ee00b11c6d51e87380e26d5
JohnWick92 <grumec92@gmail.com>
2022-12-12 10:21:55 -0300

    First tests

---
+++ src/test/unit/History.spec.ts
@@ -0,0 +1,35 @@
+import { randomUUID } from 'crypto'
+import { describe, it, expect } from 'vitest'
+import { History } from '../../entities/History'
+
+describe('History entity test switch', () => {
+  it(' should create a instancy of History', () => {
+    const history = new History({
+      code_pk: randomUUID(),
+      batch: '1',
+      code_material: '1',
+      date_of_use: new Date(),
+      description: 'test',
+      quantity_used: 1,
+      shelf_life: new Date(),
+    })
+    expect(history).toBeInstanceOf(History)
+  })
+
+  it('should throw an error if the shelf life is before date of use', () => {
+    const date_of_use = new Date()
+    const shelf_life = new Date()
+    shelf_life.setDate(shelf_life.getDate() - 1)
+    expect(() => {
+      return new History({
+        code_pk: randomUUID(),
+        batch: '1',
+        code_material: '1',
+        date_of_use,
+        description: 'test',
+        quantity_used: 1,
+        shelf_life,
+      })
+    }).toThrowError()
+  })
+})
---
+++ src/test/unit/Material.spec.ts
@@ -0,0 +1,17 @@
+import { randomUUID } from 'crypto'
+import { describe, it, expect } from 'vitest'
+import { Material } from '../../entities/Material'
+
+describe('Material entity test switch', () => {
+  it(' should create a instancy of Material', () => {
+    const material = new Material({
+      code_material: randomUUID(),
+      code_pk: '1',
+      amount: 1,
+      batch: '1',
+      description: 'test',
+      shelf_life: new Date(),
+    })
+    expect(material).toBeInstanceOf(Material)
+  })
+})
