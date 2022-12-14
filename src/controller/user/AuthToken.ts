bdccb6a7548ba7f7ad1648db4168a88094796132
JohnWick92 <grumec92@gmail.com>
2022-12-14 11:47:40 -0300

    Make token a object

--- src/controller/user/AuthToken.ts
+++ src/controller/user/AuthToken.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express'
 
 export default class AuthToken {
   async handle(request: Request, response: Response) {
-    const token = request.body
+    const { token: token } = request.body
     const AuthToken = new AuthTokenUserService()
     const user = await AuthToken.execute(token)
     return response.status(200).json(user)
