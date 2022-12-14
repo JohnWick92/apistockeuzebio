eabc031af5e6e7c5f7dbcd5db1c3b0f1ccae3bc9
JohnWick92 <grumec92@gmail.com>
2022-12-14 11:31:52 -0300

    Make login receive a correct object

--- src/controller/user/FindOne.ts
+++ src/controller/user/FindOne.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express'
 
 export default class FindOneUserController {
   async handle(request: Request, response: Response) {
-    const login = request.body
+    const { login: login } = request.body
     const findOneUserService = new FindOneUserService()
     const user = await findOneUserService.execute(login)
     return response.status(200).json(user)
