962f32405a5cff8f6284449eaa16255019b802b4
JohnWick92 <grumec92@gmail.com>
2022-12-14 11:36:09 -0300

    Make code_pk receive a correct object

--- src/controller/user/Delete.ts
+++ src/controller/user/Delete.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express'
 
 export default class DeleteUserController {
   async handle(request: Request, response: Response) {
-    const code_pk = request.body
+    const { code_pk: code_pk } = request.body
     const deleteUserService = new DeleteUserService()
     const result = await deleteUserService.execute(code_pk)
     return response.status(200).json(result)
